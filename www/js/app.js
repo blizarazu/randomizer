c41d57c784162c5027957d5d3a6f78c309fbe2f1
Beñat Lizarazu <blizarazu@users.noreply.github.com>
2014-10-24 20:41:34 +0200

    Remove unused code

--- www/js/app.js
+++ www/js/app.js
@@ -30,23 +30,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'filters'])
       controller: 'AppCtrl'
     })
 
-    .state('app.search', {
-      url: "/search",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/search.html"
-        }
-      }
-    })
-
-    .state('app.browse', {
-      url: "/browse",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/browse.html"
-        }
-      }
-    })
     .state('app.random_number', {
       url: "/random_number",
       views: {
@@ -57,26 +40,7 @@ angular.module('starter', ['ionic', 'starter.controllers', 'filters'])
       }
     })
 
-    .state('app.playlists', {
-      url: "/playlists",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlists.html",
-          controller: 'PlaylistsCtrl'
-        }
-      }
-    })
-
-    .state('app.single', {
-      url: "/playlists/:playlistId",
-      views: {
-        'menuContent' :{
-          templateUrl: "templates/playlist.html",
-          controller: 'PlaylistCtrl'
-        }
-      }
-    });
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/app/playlists');
+  $urlRouterProvider.otherwise('/app/random_number');
 });
 
